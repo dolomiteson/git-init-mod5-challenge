7a5b75bb7b4618ef8c281ea01a98068a899b3c63
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-09 07:15:09 -0600

    Add time blocks to html. Update to js

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,2 +1,29 @@
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(currentTime);
+
+/* Date/Time Funtionality */
+var dateTime = null; 
+var date = null;
+
+// Function that gets the most current date
+var currentDateTime = function(){
+    date = moment(new Date());
+    dateTime.html(date.format('MMMM Do YYYY, h:mm:ss a'));
+}
+
+// Function that  maintains accurate date and time in the heasder
+$(document).ready(function(){
+    dateTime = $("#displayMoment");
+    currentDateTime();
+    setInterval(currentDateTime, 1000);
+});
+
+/*TODO: Create Function that will create blocks for the following hours
+-0900
+-1000
+-1100
+-1200
+-1300
+-1400
+-1500
+-1600
+-1700
+*/
