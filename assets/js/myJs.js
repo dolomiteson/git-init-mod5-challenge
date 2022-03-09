65125008378ba030663137f2f35493a7760f1c03
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-09 16:32:45 -0600

    Update to add color to time blocks

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,29 +1,39 @@
 
 /* Date/Time Funtionality */
-var dateTime = null; 
-var date = null;
+var dateTime = ""; 
+var date = "";
+var hour = "";
 
 // Function that gets the most current date
-var currentDateTime = function(){
+function currentDateTime (){
     date = moment(new Date());
     dateTime.html(date.format('MMMM Do YYYY, h:mm:ss a'));
+    hour = parseInt(date.format('H'));
+    whatColor();
+}
+
+// TODO: Function to disable text Area and Button for past items 
+
+// Function to change timeblocks color in relation to current hour
+function whatColor(){
+     var timeBlockArr = $(".container").children();
+     
+    for(var index = 0; index < timeBlockArr.length; index++){
+        if(parseInt(timeBlockArr[index].id) < hour){
+            timeBlockArr[index].classList.add("bg-light");
+        }
+        else if(parseInt(timeBlockArr[index].id) === hour){
+            timeBlockArr[index].classList.add("bg-danger");
+        }
+        else{timeBlockArr[index].classList.add("bg-success");}
+    }
 }
 
 // Function that  maintains accurate date and time in the heasder
 $(document).ready(function(){
     dateTime = $("#displayMoment");
     currentDateTime();
-    setInterval(currentDateTime, 1000);
+    setInterval(currentDateTime , 1000);
 });
+ 
 
-/*TODO: Create Function that will create blocks for the following hours
--0900
--1000
--1100
--1200
--1300
--1400
--1500
--1600
--1700
-*/
